400ba612fbb4ed8dde8071c027bbb326dfd746cf
koos van losenoord <316595@student.mboutrecht.nl>
2020-03-02 12:54:28 +0100

    no message

---
+++ Tic Tac Toe/Assets/script/GameController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField] private List<Text> buttonList;
+
+    private string playerSide;
+
+    [SerializeField] private GameObject gameOverPaneel;
+
+    [SerializeField] private GameObject restartButton;
+
+    [SerializeField] private Text gameOverText;
+
+    private int moveCount;
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Count; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        //rij 1
+        moveCount++;
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        //rij 2
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        // rij 3
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        // kolom 1
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        // kolom 2
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        // kolom 3
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        // kruijs rechtsboven
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+        // kruis linksboven
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (moveCount >= 9)
+        {
+            GameOver("gelijk");
+        }
+    }
+
+    void GameOver(string winner)
+    {
+        
+    }
+
+    void SetGameOverText()
+    {
+        
+    }
+
+    void ChangeSide()
+    {
+        if (playerSide == "X")
+        {
+            playerSide = "O";
+        }
+        else
+        {
+            playerSide = "X";
+        }
+
+        playerSide = (playerSide == "X" ? "O" : "X");
+    }
+
+    void SetBoardInteractable()
+    {
+        
+    }
+
+    void RestartGame()
+    {
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tic Tac Toe/Assets/script/GridSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+
+    [SerializeField] private Button button;
+
+    [SerializeField] private Text buttonText;
+
+    private GameController gameController;
+    
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+}
